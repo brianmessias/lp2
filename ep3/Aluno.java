57c1eef08ff976cf05f480119a5cf243c3d4eefe
Brian <brian_messias@live.com>
2017-03-09 10:41:58 -0300

    Criação de ep3 : classe Aluno e Principal

---
+++ ep3/Aluno.java
@@ -0,0 +1,17 @@
+public class Aluno{
+	private String nome;
+	private int idade;
+	public void setNome (String nome){
+		this.nome = nome;
+	}
+	public void setIdade(int idade){
+		this.idade = idade;
+	}
+	public String getNome(){
+		return nome;
+	}
+	public int getIdade(){
+		return idade;
+	}
+}
+
---
+++ ep3/Principal.java
@@ -0,0 +1,24 @@
+public class Principal{
+	public static void main(String[] args){
+		Aluno a = new Aluno();
+		a.setNome("Brian");
+		a.setIdade(18);
+
+		Aluno b = new Aluno();
+		b.setNome("Birma");
+		b.setIdade(17);
+
+		Aluno c = new Aluno();
+		c.setNome("Karol");
+		c.setIdade(19);
+
+		Aluno d = new Aluno();
+		d.setNome("Matheus");
+		d.setIdade(15);
+
+
+		Aluno e = new Aluno();
+		e.setNome("Hebert");
+		e.setIdade(31);
+	}
+}
